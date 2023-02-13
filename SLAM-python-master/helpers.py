947c833c39396832c641b124b251b01a2378ac65
Jamesl3483 <jlukas6249@gmail.com>
2023-02-13 00:37:04 -0800

    Unfinished SLAM Modified
    The previous SLAM model was unusable since it did not rely on actual sensors and would only apply in a simulation environment after the robot finishes moving. Currently working on making it an animation and trying to get the simulation in motion

--- SLAM-python-master/helpers.py
+++ SLAM-python-master/helpers.py
@@ -10,7 +10,8 @@ import seaborn as sns
 # this helper function displays the world that a robot is in
 # it assumes the world is a square grid of some given size
 # and that landmarks is a list of landmark positions(an optional argument)
-def display_world(world_size, position, landmarks=None):
+def display_world(world_size, landmarks=None):
+    fig = plt.gcf()
     
     # using seaborn, set background grid to gray
     sns.set_style("dark")
@@ -19,6 +20,7 @@ def display_world(world_size, position, landmarks=None):
     world_grid = np.zeros((world_size+1, world_size+1))
 
     # Set minor axes in between the labels
+    #gca, gets current axis
     ax=plt.gca()
     cols = world_size+1
     rows = world_size+1
@@ -32,20 +34,7 @@ def display_world(world_size, position, landmarks=None):
     # Plot grid on major axes in larger width
     plt.grid(which='major',ls='-',lw=2, color='white')
     
-    # Create an 'o' character that represents the robot
-    # ha = horizontal alignment, va = vertical
-    ax.text(position[0], position[1], 'o', ha='center', va='center', color='r', fontsize=30)
-    
-    # Draw landmarks if they exists
-    if(landmarks is not None):
-        # loop through all path indices and draw a dot (unless it's at the car's location)
-        for pos in landmarks:
-            if(pos != position):
-                ax.text(pos[0], pos[1], 'x', ha='center', va='center', color='purple', fontsize=20)
-    
-    # Display final result
-    plt.show()
-
+    return fig
     
 # --------
 # this routine makes the robot data
@@ -59,67 +48,61 @@ have to create landmarks as we go. Also it seems like the goal of the
 rover is to detect all the landmarks.'''
 #########################
 
-def make_data(N, num_landmarks, world_size, measurement_range, motion_noise, 
+def make_data(r, landmarks, world_size, measurement_range, motion_noise, 
               measurement_noise, distance):
 
     # check that data has been made
     try:
-        check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise)
+        check_for_data(world_size, measurement_range, motion_noise, measurement_noise)
     except ValueError:
         print('Error: You must implement the sense function in robot_class.py.')
         return []
     
     complete = False
     
-    r = robot(world_size, measurement_range, motion_noise, measurement_noise)
-    r.make_landmarks(num_landmarks)
+    # r = robot(world_size, measurement_range, motion_noise, measurement_noise)
+    #r.detect_landmarks()
 
-    # runs until all of the landmarks are detected 
-    while not complete:
-
-        data = []
-
-        seen = [False for row in range(num_landmarks)]
+    ### runs until all of the landmarks are detected 
+    ### I am going to depend on the list of landmarks not num_of_landmarks
+    data = []
+    for i in landmarks:
+        # seen = [False for row in range(num_landmarks)]
     
-        # guess an initial motion
+        ### guess an initial motion ###
         orientation = random.random() * 2.0 * pi
         dx = cos(orientation) * distance
         dy = sin(orientation) * distance
-            
-        for k in range(N-1):
-    
+          
+        ##loops after N times which was predetermined, however, now I will loop it in an animation    
+        ##########CHANGE###############
+        for k in range(1):
+        ################################    
             # collect sensor measurements in a list, Z
             Z = r.sense()
 
-            # check off all landmarks that were observed 
-            for i in range(len(Z)):
-                seen[Z[i][0]] = True
+            # # check off all landmarks that were observed 
+            # for i in range(len(Z)):
+            #     seen[Z[i][0]] = True
     
-            # checks if movement goes out of the graph and moves in a direction
-            while not r.move(dx, dy):
-                # if we'd be leaving the robot world, pick instead a new direction
-                orientation = random.random() * 2.0 * pi
-                dx = cos(orientation) * distance
-                dy = sin(orientation) * distance
+            # checks if movement goes out of the graph and moves in a direction if it is
+            r.move(dx,dy)
 
             # collect/memorize all sensor and motion data
             data.append([Z, [dx, dy]])
 
-        # we are done when all landmarks were observed; otherwise re-run
-        complete = (sum(seen) == num_landmarks)
 
     print(' ')
     print('Landmarks: ', r.landmarks)
     print(r)
 
-
     return data
 
 
-def check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise):
+def check_for_data(world_size, measurement_range, motion_noise, measurement_noise):
     # make robot and landmarks
-    r = robot(world_size, measurement_range, motion_noise, measurement_noise)
-    r.make_landmarks(num_landmarks)
+    r = robot(world_size, measurement_range)
+    # r.make_landmarks(num_landmarks)
     
     
     # check that sense has been implemented/data has been made
--- SLAM-python-master/robot_class.py
+++ SLAM-python-master/robot_class.py
@@ -7,7 +7,7 @@ import random
 #
 # This robot lives in 2D, x-y space, and its motion is
 # pointed in a random direction, initially.
-# It moves in a straight line until it comes close to a wall 
+# It moves in a straight line until it comes close to a wall
 # at which point it stops.
 #
 # For measurements, it  senses the x- and y-distance
@@ -17,46 +17,42 @@ import random
 # cluttered math.
 #
 class robot:
-    
+
     # --------
     # init:
     #   creates a robot with the specified parameters and initializes
     #   the location (self.x, self.y) to the center of the world
     #
-    def __init__(self, world_size = 100.0, measurement_range = 30.0,
-                 motion_noise = 1.0, measurement_noise = 1.0):
-        self.measurement_noise = 0.0
+    def __init__(self, world_size=100.0, measurement_range=30.0):
         self.world_size = world_size
         self.measurement_range = measurement_range
         self.x = world_size / 2.0
         self.y = world_size / 2.0
-        self.motion_noise = motion_noise
-        self.measurement_noise = measurement_noise
         self.landmarks = []
-        self.num_landmarks = 0
-    
-    
+        self.distance = 20.0 #The size of the step that the robot takes
+
     # returns a positive, random float
+
     def rand(self):
         return random.random() * 2.0 - 1.0
-    
-    
+
     # --------
     # move: attempts to move robot by dx, dy. If outside world
     #       boundary, then the move does nothing and instead returns failure
-    #
+
     def move(self, dx, dy):
+
+        x = self.x + dx
+        y = self.y + dy
+
+        while x < 0.0 or x > self.world_size or y < 0.0 or y > self.world_size:
+            orientation = random.random() * 2.0 * pi
+            dx = cos(orientation) * self.distance
+            dy = sin(orientation) * self.distance
         
-        #Makes its own errors in movement through the noise
-        x = self.x + dx + self.rand() * self.motion_noise
-        y = self.y + dy + self.rand() * self.motion_noise
-        
-        if x < 0.0 or x > self.world_size or y < 0.0 or y > self.world_size:
-            return False
-        else:
-            self.x = x
-            self.y = y
-            return True
+        self.x = x
+        self.y = y
+
 
 
     # --------
@@ -66,46 +62,49 @@ class robot:
     #        landmarks to be visible at all times
     #
     
-    ## TODO: paste your complete the sense function, here
-    ## make sure the indentation of the code is correct
+    # TODO: paste your complete the sense function, here
+    # make sure the indentation of the code is correct
     
     ##
     def sense(self):
         ''' This function list the distances of the landmarks within the
-        the sensing radius(measurement_range)
+        the sensing radius(measurement_range). It is good since it 
+        keeps a list of measurements of distance from each landmark during every
+        run time
             '''
            
         measurements = []
         
-        ## TODO: iterate through all of the landmarks in a world
-        
-        ## TODO: For each landmark
-
-        for index in range(self.num_landmarks):
+        # TODO: iterate through all of the landmarks in a world
+        for index in range(len(self.landmarks)):
             # distance between landmark and rover
             dist_x = self.landmarks[index][0] - self.x
             dist_y = self.landmarks[index][1] - self.y
-            # add noise
-            noise = self.rand() * self.measurement_noise
-            dist_x += noise
-            dist_y += noise
-            #check if landmark is in range
+            # check if landmark is in range
             if(abs(dist_x) < self.measurement_range and abs(dist_y) < self.measurement_range):
                 measurements.append([index, dist_x, dist_y])
-        ## TODO: return the final, complete list of measurements
+        # TODO: return the final, complete list of measurements
         return measurements
 
     # --------
     # make_landmarks:
     # make random landmarks located in the world
-    #
-    def make_landmarks(self, num_landmarks):
+    '''def make_landmarks(self, num_landmarks):
         self.landmarks = []
         for i in range(num_landmarks):
             self.landmarks.append([round(random.random() * self.world_size),
                                    round(random.random() * self.world_size)])
-        self.num_landmarks = num_landmarks
+        self.num_landmarks = num_landmarks'''
 
+    # make_landmarks:
+    ###############CHANGE#############
+    def detect_landmarks(self):
+    # use sensor to determine place of obstacle and create the landmark
+        self.landmarks
+        # planned: appends a list of the placement of landmarks and records them
+    
+    ##############CHANGE###############
+        
 
     # called when print(robot) is called; prints the robot's location
     def __repr__(self):
