66f65888269ead427d238b42ce3eec57dce39267
Jamesl3483 <jlukas6249@gmail.com>
2023-03-10 23:14:35 -0800

    True randomized walls
    Created a random wall algorithm and able to dynamically expand the number of robots

--- simulation_attempt.py
+++ simulation_attempt.py
@@ -13,18 +13,29 @@ num_landmarks = 6
 running = True
 world_size = 100.0    # size of world (square)
 
-# make_landmarks:
 # # make random landmarks located in the world
 def make_landmarks(num_landmarks):
-    #for random obstacles
-    # for i in range(num_landmarks):
-    #     landmarks.append([round(random.random() * world_size),round(random.random() * world_size)])
+    #pick random start point
+    for i in range(num_landmarks):
+        x_coor = round(random.random() * world_size)
+        y_coor = round(random.random() * world_size)
+        while x_coor > world_size - 20:
+            x_coor = round(random.random() * world_size) 
+        while y_coor > world_size - 20:
+            y_coor = round(random.random() * world_size)
+            
+        #Horizontal Wall
+        if random.random() <= .5:
+            for i in range(5):
+                landmarks.append([y_coor, x_coor + i*4])
+        else:
+            for i in range(5):
+                landmarks.append([y_coor + i*4, x_coor])
+        
     
     #for a wall
     #horizontal_coor = round(random.random() * world_size)
     horizontal_coor = 60
-    for i in range(round(world_size/5)):
-        landmarks.append([horizontal_coor, i*5])
         
     #You can import a maze if you want make it more complex
     
@@ -38,7 +49,7 @@ distance = 1.0
 
 #Initiate robot class
 landmarks = make_landmarks(num_landmarks)
-num_of_robots = 3
+num_of_robots = 8
 robot_list = []
 for i in range(num_of_robots):
     robot_list.append(robot(world_size, measurement_range, landmarks))
@@ -51,44 +62,33 @@ def print_positions(poses):
     print('Estimated Poses:')
     print('['+ str(poses[0]) + ', ' + str(poses[1]) + ']')
     print('\n')
-    # print('Estimated Landmarks:')
-    # for i in range(len(landmarks)):
-    #     print('['+', '.join('%.3f' % l for l in landmarks[i])+']')
-# i = 0
-# while i < time_steps:
-#     # print(i)
-#     for j in range(len(robot_list)):
-#         robot_list[j].detect_landmarks()
-#         robot_list[j].move()
-        # print_positions(robot_list[j].record_movement[-1])
-
-    # if keyboard.is_pressed('escape'):
-    #     running = False
     i+=1
 
 
 #2D Array of empty world
 world = np.zeros((int(world_size), int(world_size)))
 
+#Graph Landmarks, get rid of it after testing period
+b = [[particle[0] for particle in landmarks], [
+            particle[1] for particle in landmarks]]
+for i in range(len(b[0])):
+        world[int(b[0][i])][int(b[1][i])]=50
 
 
-# create the plot layout
-####TASK######
-#Make the number of plots dynamic to the number of robots
-##############
-fig, axes = plt.subplots(2,2)
-
-
-#get graph color
-def get_cmap(n, name='hsv'):
-    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
-    RGB color; the keyword argument name must be a standard mpl colormap name.'''
-    return plt.cm.get_cmap(name, n)
-
-
-
-def animate(data):
+# Optimize format of multiple graphs
+num_of_graphs = num_of_robots + 1
+vertical_graph_stack = int(sqrt(num_of_graphs))
+horizontal_graph_stack = vertical_graph_stack
+graph_spaces = vertical_graph_stack*horizontal_graph_stack
+while num_of_graphs > graph_spaces:
+    horizontal_graph_stack += 1
+    graph_spaces = vertical_graph_stack*horizontal_graph_stack
     
+# create the plot layout
+fig, axes = plt.subplots(vertical_graph_stack,horizontal_graph_stack)
+
+#animate the graphs
+def animate(data):
     for index in range(len(robot_list)):
         empty_world = np.zeros((int(world_size), int(world_size)))
         robot_list[index].detect_landmarks()
@@ -106,18 +106,20 @@ def animate(data):
                 empty_world[int(b[0][i])][int(b[1][i])]=50
         
         fig.axes[index].clear()
+        #If want different colormap, add cmap in this parameters
         fig.axes[index].matshow(empty_world)
             
     fig.axes[-2].clear()
     fig.axes[-2].matshow(world)
 
 
-
+#Initialize the graphs
 for ax in axes.flat:
-    im = ax.matshow(world, vmin=0, vmax=100)
+    #If want different colormap, add cmap in this parameters
+    im = ax.imshow(world, vmin=0, vmax=100)
 
-
-
+#Set Color map
+# fig.colorbar(im,ax=axes.ravel().tolist())
 fig.colorbar(im,ax=axes.ravel().tolist())
 
 ani = animation.FuncAnimation(fig, animate, frames = 500, interval=1)
